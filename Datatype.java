ce31ca3ab959d247c0d98d6654a10b40df48a5c9
sarvamangala-c <sarvamangala123456789@gmail.com>
2025-04-09 16:05:14 +0530

    save

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+package cie1;
+
+public class Datatype {
+    public static void main (String[] args){
+        int Num=5;
+        float Float=5.44f;
+        char ch='D';
+        boolean myBool=true;
+        String str="Hello";
+        long Long=54_58655;
+        System.out.println("Num: "+Num);
+        System.out.println("Float: "+Float);
+        System.out.println("character: "+ch);
+        System.out.println("Boolean: "+myBool);
+        System.out.println("String: "+str);
+        System.out.println("long: "+Long);
+        
+    }
+}
